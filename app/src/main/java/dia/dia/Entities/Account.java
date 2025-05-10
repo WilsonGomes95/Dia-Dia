be6ec0bf61eb7218f04b78953a054cc79e5c0f92
Wilson Gomes <69987793+WilsonGomes95@users.noreply.github.com>
2025-05-10 20:25:35 +0000

    Adiciona a classe Account e implementa funcionalidades de depósito e saque

---
+++ app/src/main/java/dia/dia/Entities/Account.java
@@ -0,0 +1,65 @@
+package dia.dia.Entities;
+
+public class Account {
+    private String number;
+    private String holder;
+    private Double balance;
+    private Double withdrawLimit;
+    
+    public Account(){}
+    
+    public Account(String number, String holder, double balance, double withdrawLimit) {
+        this.number = number;
+        this.holder = holder;
+        this.balance = balance;
+        this.withdrawLimit = withdrawLimit;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public String getHolder() {
+        return holder;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public double getWithdrawLimit() {
+        return withdrawLimit;
+    }
+
+    public void deposit(double deposit){
+        if (deposit <= 0){
+            throw new IllegalArgumentException("O valor do depósito deve ser maior que zero.");
+        }
+        this.balance += deposit;
+
+    }
+
+    public void withdraw(double withdraw){
+        if (withdraw > this.getBalance()){
+            throw new IllegalArgumentException("Valor de saque maior que o saldo.");
+        }
+        if (withdraw <= 0){
+            throw new IllegalArgumentException("O valor do saque deve ser maior que zero.");
+        }
+
+        this.balance -= withdraw;
+
+    }
+
+
+    @Override
+    public String toString(){
+        return "Number: " + this.getNumber() + "\n" +
+                "Holder: " + this.getHolder() + "\n" +
+                "Balance: " + String.format("%.2f", this.getBalance()) + "\n" +
+                "Withdraw limit: " + String.format("%.2f", this.getWithdrawLimit());
+    }
+
+    
+
+}
